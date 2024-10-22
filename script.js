c794d54e4b3eaba8a514e8d2e3e78289923cccad
jalajpatel <96616944+jalajpatel@users.noreply.github.com>
2024-10-22 13:18:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function navigateToInitiative(initiative) {
+    alert(`Learn more about ${initiative}!`);
+    // Replace the alert with actual navigation if needed, like:
+    // window.location.href = `${initiative}.html`;
+}
